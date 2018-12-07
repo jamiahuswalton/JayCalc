c913572759b1817f7c3734790657cb4518420348
jamiahuswalton <jamiahuswalton@gmail.com>
2018-12-07 08:53:50 -0600

    Add the ADD calculation method

--- app/src/main/java/com/jamiahus/jaycalc/OperationContainer.java
+++ app/src/main/java/com/jamiahus/jaycalc/OperationContainer.java
@@ -30,11 +30,21 @@ public class OperationContainer {
     public String CalculateEquation(){
         String result = "Error";
         float tempNumber;
-        for (OperationBase calculationTpye: ListOfOperations) {
+
+        for (int i = 0; i < ListOfOperations.size(); i++){
+            OperationBase calculationTpye = ListOfOperations.get(i);
             if (calculationTpye.GetOperationType() == OperationType.Number){
                 OperationNumber currentNumber = (OperationNumber)calculationTpye;
                 tempNumber = Float.parseFloat(currentNumber.GetRunningNumber());
                 Log.d("Calculation", String.valueOf(tempNumber));
+            } else if (calculationTpye.GetOperationType() == OperationType.Add){
+                OperationNumber previousNumber = (OperationNumber) ListOfOperations.get(i-1);
+                OperationNumber nextNumber = (OperationNumber) ListOfOperations.get(i+1);
+                float previousNumberFloat = Float.parseFloat(previousNumber.GetRunningNumber());
+                float nextNumberFloat = Float.parseFloat(nextNumber.GetRunningNumber());
+                float additionResult = previousNumberFloat + nextNumberFloat;
+                Log.d("Results", String.valueOf(additionResult));
+                //TODO: Test to make sure this is working properly.
             }
             //TODO: Need to add code to handle the different operations (e.g., Add, subtract, etc.)
         }
